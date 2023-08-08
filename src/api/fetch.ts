a22dc585962885408bd7fb48a2455c022efa0e9a
Ilgar <IlgarAbdullazade>
2023-08-08 13:22:26 +0400

    add api folder

---
+++ src/api/fetch.ts
@@ -0,0 +1,16 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import axios from './interceptors'
+
+type TypeInput = {
+  url: string
+  method?: 'GET' | 'POST' | 'PUT' | 'DELETE'
+  body?: any
+}
+
+export const api = async ({ url, method, body }: TypeInput) => {
+  return axios({
+    method,
+    url,
+    data: body,
+  })
+}
