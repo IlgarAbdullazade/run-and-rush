8f4223a50f6af28294675b0b518eec93b01e407b
Ilgar <IlgarAbdullazade>
2023-08-18 14:10:46 +0400

    Add account url

--- src/configs/apiConfig.ts
+++ src/configs/apiConfig.ts
@@ -3,3 +3,4 @@ export const API_URL = `${process.env.NEXT_PUBLIC_API_URL}/api`
 export const getAuthUrl = (string: string) => `/auth${string}`
 export const getUsersUrl = (string: string) => `/users${string}`
 export const getAccountUrl = (string: string) => `/account${string}`
+export const getReferralUrl = (string: string) => `/referral${string}`
